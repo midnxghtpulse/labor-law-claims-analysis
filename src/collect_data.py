bb3a6950c672d76c38f896ae3412f84c6eff1c86
jd! <joaodavisouzafarias@gmail.com>
2026-08-20 16:51:42 -0300

    update collect_data.py

--- src/collect_data.py
+++ src/collect_data.py
@@ -1,1 +1,25 @@
-print("datajud collector")
+import requests
+import json
+
+url = "https://api-publica.datajud.cnj.jus.br/api_publica_trt5/_search"
+
+headers = {
+    "Authorization": "APIKey cDZHYzlZa0JadVREZDJCendQbXY6SkJlTzNjLV9TRENyQk1RdnFKZGRQdw==",
+    "Content-Type": "application/json"
+}
+
+query = {
+    "size": 5,
+    "query": {
+        "match_all": {}
+    }
+}
+
+response = requests.post(
+    url,
+    headers=headers,
+    data=json.dumps(query)
+)
+
+print("status:", response.status_code)
+print(response.text)
