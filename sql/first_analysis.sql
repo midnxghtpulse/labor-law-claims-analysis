885a3a13c91e4b40d49f498a5f57a59b8ece00ae
jd! <joaodavisouzafarias@gmail.com>
2026-08-31 15:02:33 -0300

    update first_analysis.sql

--- sql/first_analysis.sql
+++ sql/first_analysis.sql
@@ -50,3 +50,40 @@ where dano_moral = 1;
 
 -- court dna [selected theme incidence by judging body]
 
+select
+    judging_body,
+
+    count(*) as total_cases,
+
+    round(
+        100.0 * sum(horas_extras) / count(*),
+        2
+    ) as pct_horas_extras,
+
+    round(
+        100.0 * sum(dano_moral) / count(*),
+        2
+    ) as pct_dano_moral,
+
+    round(
+        100.0 * sum(fgts) / count(*),
+        2
+    ) as pct_fgts,
+
+    round(
+        100.0 * sum(aviso_previo) / count(*),
+        2
+    ) as pct_aviso_previo,
+
+    round(
+        100.0 * sum(verbas_rescisorias) / count(*),
+        2
+    ) as pct_verbas_rescisorias
+
+from labor_claims
+
+group by judging_body
+
+having count(*) >= 50
+
+order by total_cases desc;
